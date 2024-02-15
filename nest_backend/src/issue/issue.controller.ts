539f3b51eff98826ab3dbad7cd91d38d5a316227
johnvergilaparriSA <johnvergil.aparri@sun-asterisk.com>
2024-02-15 18:24:44 +0800

    backend: cleaned some unused code

--- nest_backend/src/issue/issue.controller.ts
+++ nest_backend/src/issue/issue.controller.ts
@@ -5,35 +5,6 @@ import { ConfigService } from '@nestjs/config';
 import { Request } from 'express';
 
 
-async function getPaginatedData(url:string, octokit:Octokit) {
-  const nextPattern = /(?<=<)([\S]*)(?=>; rel="Next")/i;
-  let pagesRemaining = true;
-  let data = [];
-
-  while (pagesRemaining) {
-    const response = await octokit.request(`GET ${url}`, {
-      per_page: 100,
-      headers: {
-        "X-GitHub-Api-Version":
-          "2022-11-28",
-      },
-    });
-
-    const parsedData = parseData(response.data)
-    data = [...data, ...parsedData];
-
-    const linkHeader = response.headers.link;
-
-    pagesRemaining = linkHeader && linkHeader.includes(`rel=\"next\"`);
-
-    if (pagesRemaining) {
-      url = linkHeader.match(nextPattern)[0];
-    }
-  }
-
-  return data;
-}
-
 function getLabelArray(labels:{
     id: number,
     node_id: string,
@@ -60,30 +31,6 @@ function getLabelArray(labels:{
         })
         return labelArr;
     }
-}
-
-function parseData(data) {
-  // If the data is an array, return that
-    if (Array.isArray(data)) {
-      return data
-    }
-
-  // Some endpoints respond with 204 No Content instead of empty array
-  //   when there is no data. In that case, return an empty array.
-  if (!data) {
-    return []
-  }
-
-  // Otherwise, the array of items that we want is in an object
-  // Delete keys that don't include the array of items
-  delete data.incomplete_results;
-  delete data.repository_selection;
-  delete data.total_count;
-  // Pull out the array of items
-  const namespaceKey = Object.keys(data)[0];
-  data = data[namespaceKey];
-
-  return data;
 }
 
 @Controller('/')
